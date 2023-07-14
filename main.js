04d78f41012d4da121bba1f630db7b2f604670a5
Timur Tigiyev <tigiyev@gmail.com>
2023-07-14 17:47:37 +0400

    lesson ended

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const product = 'Socks'
+// const product = 'Socks'
+
+const app = Vue.createApp({
+	data() {
+		return {
+			product: 'Mouth',
+			description: 'Im only human after all!',
+			test() {
+				return 'yoo'
+			},
+			test2: function () {
+				return 'yoo2'
+			}
+		}
+	}
+});
