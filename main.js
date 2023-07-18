15d33062c5c9fad0a00521bddf3dd82daa426b37
Timur Tigiyev <tigiyev@gmail.com>
2023-07-18 16:12:57 +0400

    Lesson 10 completed

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            premium: true
-        }
-    },
-    methods: {}
+	data() {
+		return {
+			cart: [],
+			premium: true
+		}
+	},
+	methods: {
+		updateCart(id) {
+			this.cart.push(id)
+		},
+		removeFromCart(id) {
+			this.cart.pop()
+		}
+	}
 })
